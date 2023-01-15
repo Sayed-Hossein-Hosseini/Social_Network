6132d5d3a98cfbbd91fa636d404ec71d7e0a2e97
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2023-01-16 01:30:50 +0330

    Creating a user interface

---
+++ SocailNetwork/src/IUser.java
@@ -0,0 +1,10 @@
+public interface IUser {
+    /** Return element associated with this vertex. */
+    String getName();
+    String getFamily();
+    String getEmail();
+    String getPhoneNumber();
+    String getUserName();
+    String getPassword();
+    String getID();
+}
